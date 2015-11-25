a02fd34717639bb77590dc541cec0217a510ddaa
Chuy Del Castillo <chuy@imchuy.com>
2015-11-25 10:14:06 -0700

    2.29 solution

--- chapter2/2.29-binary-mobile.scm
+++ chapter2/2.29-binary-mobile.scm
@@ -1,20 +1,24 @@
 (define (make-mobile left right)
-  (list left right))
+  ;; (list left right))
+  (cons left right))
 
 (define (make-branch length structure)
-  (list length structure))
+  ;; (list length structure))
+  (cons length structure))
 
 (define (left-branch mobile)
   (car mobile))
 
 (define (right-branch mobile)
-  (car (cdr mobile)))
+  ;; (car (cdr mobile)))
+  (cdr mobile)) ;; We leave car when using cons for constructing branches/mobiles
 
 (define (branch-length branch)
   (car branch))
 
 (define (branch-structure branch)
-  (car (cdr branch)))
+  ;; (car (cdr branch)))
+  (cdr branch)) ;; We leave car when using cons for constructing branches/mobiles
 
 (define (total-weight mobile)
   (+ (branch-weight (left-branch mobile))
