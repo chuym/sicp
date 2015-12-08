461f717addeffb21796e191a627cd264cb28a6e8
Chuy Del Castillo <chuy@imchuy.com>
2015-12-08 12:59:49 -0700

    2.38 solution

---
+++ chapter2/2.38-fold-operations.scm
@@ -0,0 +1,16 @@
+(define (fold-left op initial sequence)
+  (define (iter result rest)
+    (if (null? rest)
+        result
+        (iter (op result (car rest))
+              (cdr rest))))
+  (iter initial sequence))
+
+(define (fold-right op initial sequence)
+  (if (null? sequence)
+      initial
+      (op (car sequence)
+          (fold-right op initial (cdr sequence)))))
+
+;; In order for fold-left and fold-right producing the same result, then op must the
+;; commutative property, that is (= (op a b) (op b a)).
