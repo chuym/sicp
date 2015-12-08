84eb4c153be162f08ecabb249de3b105c7f2f61c
Chuy Del Castillo <chuy@imchuy.com>
2015-12-08 13:09:52 -0700

    2.39 solution

---
+++ chapter2/2.39-reverse.scm
@@ -0,0 +1,5 @@
+(define (reverse-right sequence)
+  (fold-right (lambda (x y) (append y (list x))) '() sequence))
+
+(define (reverse-left sequence)
+  (fold-left (lambda (x y) (append (list y) x)) '() sequence))
